95d45195f8ff18fd2c1a058639e1bace6300bf80
RamyElFarou <ramyelfarou@gmail.com>
2016-06-18 14:04:49 +0000

    modify readme for repo

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   root 'welcome#home'
-  get 'about', to:'welcome#about'
+  get 'about', to:'welcome#git -- vrabout'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
