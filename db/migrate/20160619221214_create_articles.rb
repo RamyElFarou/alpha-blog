921af96b1e5be64a90059b0606b6298e7b101f97
RamyElFarou <ramyelfarou@gmail.com>
2016-07-16 21:11:16 +0000

    create users table and model

--- db/migrate/20160619221214_create_articles.rb
+++ db/migrate/20160619221214_create_articles.rb
@@ -5,3 +5,8 @@ class CreateArticles < ActiveRecord::Migration
     end
   end
 end
+
+#command rails generate migration create_articles ... predefined method for creating a db table. since the
+#    table needs to columns (title, description), we must create 2 migration files. 
+#    this command creats a migration file (this file) under db/migrate
+#    Active Record tracks which migrations have already been run so all you have to do is update your source and run rake db:migrate. Active Record will work out which migrations should be run. It will also update your db/schema.rb file to match the structure of your database.#
